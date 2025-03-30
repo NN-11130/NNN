3fea9aff8178d1f12c24ba0d23717f66bccb53fc
NN-11130 <1304940@wku.edu.cn>
2025-03-30 16:48:56 +0800

    Initial commit: Loan Qualifier app

---
+++ clock.py
@@ -0,0 +1,18 @@
+from tkinter import *
+from tkinter.ttk import *
+from time import strftime
+
+myclock=Tk()
+myclock.title('Clock')
+
+def time():
+    string=strftime("%H:%M:%S %p")
+    label.config(text=string)
+    label.after*(1000,time)
+    
+
+label=Label(myclock,font=("Aeial Rounded Bold",80))
+label.pack(anchor="center")
+
+time()
+mainloop()
